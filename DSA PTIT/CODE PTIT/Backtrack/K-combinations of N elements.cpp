8bc8af9badbcb24c78ebf461812f9d8813057fdb
Dat <whisperkuu.41@gmail.com>
2025-03-24 11:33:33 +0700

    Create  K-combinations of N elements.cpp

---
+++ DSA PTIT/CODE PTIT/Backtrack/K-combinations of N elements.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,k, x[1001];
+void display()
+{
+	for(int i = 1; i<= k; i++)
+	{
+		cout << x[i];
+	}
+	cout <<endl;
+}
+void Try(int i)
+{
+	for(int j = x[i-1]+1; j<= n-k+i; j++)
+	{
+		x[i] = j;
+		if(i == k)
+			display();
+		else
+		{
+			Try(i+1);
+		}
+	}
+}
+int main()
+{
+
+	cin >> n >> k;
+	x[0] = 0;
+	Try(1);
+}
