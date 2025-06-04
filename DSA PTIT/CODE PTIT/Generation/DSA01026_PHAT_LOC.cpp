7f2d436884fd99ec95a83b326044b1514bfeda12
Dat <193557964+cupeedrl@users.noreply.github.com>
2025-06-04 16:59:26 +0700

    Update DSA01026_PHAT_LOC.cpp

--- DSA PTIT/CODE PTIT/Generation/DSA01026_PHAT_LOC.cpp
+++ DSA PTIT/CODE PTIT/Generation/DSA01026_PHAT_LOC.cpp
@@ -1,21 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k,check; 
+int n, k; 
 string str;
+int check;
 
 void initial()
 {
-	for(int i = 0; i<k; i++)
-    {
-        str[i] = 'A' + i;
-    }
+	str = string(n, '6');
 }
-void generate() 
-{   
-    int i = n;
-    while(i>= 0 && str[i] == n - k + i)
+void generate() {
+    int i = str.length()-1;
+    while(str[i] == '8')
     {
+        str[i] = '6';
         i--;
     }
     if(i < 0)
@@ -25,29 +23,39 @@ void generate()
     }
     else
     {
-        str[i]++;
-        for(int j = i+1 ; j < n; j++)
-        {
-            str[j] = str[j-1]+1;
-        }
+        str[i] ='8';
     }
 }
-
+int inspect(string &str)
+{
+    if(str[0] == '6' || str[n-1] == '8')
+        return 0;
+    for(int i = 0; i < n; i++) 
+    {
+        if(str[i] == '8' && str[i+1] == '8')
+            return 0;
+    }
+    for(int i = 0; i <n-3; i++) 
+    {
+        if(str[i] == '6' && str[i+1] == '6' && str[i+2] == '6' && str[i+3] == '6')
+            return 0;
+    }
+    return 1;
+}
 int main() {
 
-    int t;
-    cin >> t;
-    while(t--)
-    {
-        cin >> n >> k;
+        cin >> n;
+
         initial();
+
 		check = 0;
         while(check == 0)
         {
-            cout<<str;
             generate();
-            
-        }        
-    }
+            if(inspect(str) == 1)
+            {
+            	cout<<str<<endl;
+			}
+        }
     return 0;
 }    
