f20f8278e3016de08cc46d8341eabed767effcc1
Dat <193557964+cupeedrl@users.noreply.github.com>
2025-06-05 23:44:07 +0700

    Update DSA05001_XAU_CON_CHUNG_DAI_NHAT.cpp

--- DSA PTIT/CODE PTIT/Dynamic Programming/DSA05001_XAU_CON_CHUNG_DAI_NHAT.cpp
+++ DSA PTIT/CODE PTIT/Dynamic Programming/DSA05001_XAU_CON_CHUNG_DAI_NHAT.cpp
@@ -5,20 +5,20 @@ int dynamic_programm(string s1, string s2)
 {
     int n = s1.size();
     int m = s2.size();
-    int ans[n+1][m+1]; 
+    int dp[n+1][m+1]; 
 
     for(int i = 0; i <= n; i++) // take equal sign cause the first row,col
     {                           // must be used to store 000000
         for(int j = 0; j <= m; j++)
         {
             if( i == 0 || j == 0)
-                ans[i][j] = 0;
+                dp[i][j] = 0;
             else
             {
                 if(s1[i-1] == s2[j-1])
-                    ans[i][j] = ans[i-1][j-1] + 1;
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 else
-                    ans[i][j] = max(ans[i][j-1],ans[i-1][j]);
+                    dp[i][j] = max(ans[i][j-1],ans[i-1][j]);
             }
         } 
     }
