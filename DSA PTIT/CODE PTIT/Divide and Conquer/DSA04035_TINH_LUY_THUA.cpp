30cc34a5ee7c1b9ba06611fbd568d2589780f40a
Dat <193557964+cupeedrl@users.noreply.github.com>
2025-06-05 18:58:23 +0700

    Update DSA04035_TINH_LUY_THUA.cpp

--- DSA PTIT/CODE PTIT/Divide and Conquer/DSA04035_TINH_LUY_THUA.cpp
+++ DSA PTIT/CODE PTIT/Divide and Conquer/DSA04035_TINH_LUY_THUA.cpp
@@ -1,28 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-int a,b;
+int a;
+long long b;
 const long long mod = 1e9+7;
 
-long long number_pow(int a, int b)
+long long number_pow(int a, long long b)
 {
-	if(b == 1)
-		return a % mod;
+	if(b == 0 )	return 1;
+ 	if(b == 1)	return a;
+	
 	long long tmp =  number_pow(a,b/2);
+	
 	if(b % 2 == 1)
-		return (tmp * tmp * a) % mod;
+		return ((tmp * tmp) % mod * a) % mod;
 	else	
 		return (tmp * tmp)% mod;
+		
 }
 
 int main()
 {
-    do 
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+   	while(true)
     {
 		cin >> a >> b;
-        if (a != 0 && b != 0)
-		    cout << number_pow(a,b) << endl; 
+        if (a == 0 && b == 0)
+		    return 0;
+		else
+			cout << number_pow(a,b) << endl;
     }
-	while(a != 0 && b != 0);
 
 }
-
