e66fedb4d43fd35c1785d7237a27191eef1ebac0
Dat <193557964+cupeedrl@users.noreply.github.com>
2025-06-05 13:21:20 +0700

    Create DSA3005_CHIA_MANG_THANH_2_MANG_CON_CO_TONG_MAX.cpp

---
+++ DSA PTIT/CODE PTIT/Greedy Algorithm/DSA3005_CHIA_MANG_THANH_2_MANG_CON_CO_TONG_MAX.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int n,k;
+        cin >> n >> k;
+        int a[n+1];
+        int ans =0;
+        for(int i = 1; i<=n;i++) cin >> a[i];
+        sort(a+1,a+n+1);
+		if(k < n-k)
+		{
+			for(int i = 1; i<=k;i++) ans-=a[i];
+			for(int i = k+1; i<=n;i++) ans+=a[i];
+		}
+		else
+		{
+			for(int i = 1; i<=n-k;i++) ans-=a[i];
+			for(int i = n-k+1; i<=n;i++) ans+=a[i];
+		}
+			
+        cout << ans<< endl;
+    }
+    return 0;
+}
