3527b74e4c9a4b79e9f093903c44a8fdb5b4d633
Dat <193557964+cupeedrl@users.noreply.github.com>
2025-06-06 09:12:26 +0700

    Update DSA05027_CAI_TUI.cpp

--- DSA PTIT/CODE PTIT/Dynamic Programming/DSA05027_CAI_TUI.cpp
+++ DSA PTIT/CODE PTIT/Dynamic Programming/DSA05027_CAI_TUI.cpp
@@ -13,11 +13,12 @@ int napsack()
     int dp[1005][1005];
     memset(dp,0,sizeof(dp)); // initial
     // dp[i][j]: consider first i items, backpack capacity j
-    for(int i = 1; i<= n;i++) // i = 0; dont need to choose, start with 1
+    for(int i = 1; i<= n;i++)
     {
-        for(int j = 0; j<= v;j++) 
+        for(int j = 1; j<= v;j++) 
         {
             dp[i][j] = dp[i-1][j]; // not choose;
+
             if(j >= items[i].weight ) // capacity is available, choose
             {
                 dp[i][j] = max(dp[i][j], dp[i-1][j-items[i].weight]+items[i].value);
